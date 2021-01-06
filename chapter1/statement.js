67fbf72e885fdb4ff47c6275c5eb2e0983df5177
張正穎 <chengying.chang@junyiacademy.org>
2021-01-07 01:26:47 +0800

    split files

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -1,77 +1,10 @@
+const { createStatementData } = require('./createStatementData');
+
 var plays = require('./__mocks__/plays.json');
 var invoices = require('./__mocks__/invoices.json');
 
 function statement(invoice, plays) {
 	return renderPlainText(createStatementData(invoice, plays));
-}
-
-function createStatementData(invoice, plays) {
-	const result = {};
-	result.customer = invoice.customer;
-	result.performances = invoice.performances.map(enrichPerformance);
-	result.totalAmount = totalAmount(result);
-	result.totalVolumeCredits = totalVolumeCredits(result);
-	return result;
-
-	function enrichPerformance(aPerformance) {
-		const result = Object.assign({}, aPerformance);
-		result.play = playFor(result);
-		result.amount = amountFor(result);
-		result.volumeCredits = volumeCreditsFor(result);
-		return result;
-	}
-
-	function totalVolumeCredits(data) {
-		let volumeCredits = 0;
-		for (let perf of data.performances) {
-			volumeCredits += perf.volumeCredits;
-		}
-		return volumeCredits;
-	}
-
-	function totalAmount(data) {
-		let result = 0;
-		for (let perf of data.performances) {
-			result += perf.amount;
-		}
-		return result;
-	}
-
-	function volumeCreditsFor(aPerformance) {
-		let result = 0;
-		result += Math.max(aPerformance.audience - 30, 0);
-		if ('comedy' === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-		return result;
-	}
-
-	function playFor(aPerformance) {
-		return plays[aPerformance.playID];
-	}
-
-	function amountFor(aPerformance) {
-		let result = 0;
-
-		switch (aPerformance.play.type) {
-			case 'tragedy':
-				result = 40000;
-				if (aPerformance.audience > 30) {
-					result += 1000 * (aPerformance.audience - 30);
-				}
-				break;
-
-			case 'comedy':
-				result = 30000;
-				if (aPerformance.audience > 20) {
-					result += 10000 + 500 * (aPerformance.audience - 20);
-				}
-				result += 300 * aPerformance.audience;
-				break;
-
-			default:
-				throw new Error(`unknown type: ${aPerformance.play.type}`);
-		}
-		return result;
-	}
 }
 
 function renderPlainText(data) {
